6769b05e0aea07a48da8ade28988a70ce52050ef
Andrew Woods <services@awoods.net>
2024-11-06 15:17:15 -0500

    Add title case method and unit tests
    Initial development for the title case method.

--- src/Content.php
+++ src/Content.php
@@ -4,9 +4,76 @@ namespace AndrewWoods\ChicagoStyle;
 
 class Content
 {
+    protected $articles = ['a', 'an', 'the'];
+
+    protected $conjunctions = ['and', 'but', 'for', 'nor', 'or', 'so', 'yet'];
+
+    protected $prepositions = [
+        'about',
+        'above',
+        'across',
+        'after',
+        'against',
+        'among',
+        'around',
+        'at',
+        'before',
+        'behind',
+        'below',
+        'beside',
+        'between',
+        'by',
+        'down',
+        'during',
+        'for',
+        'from',
+        'in',
+        'inside',
+        'into',
+        'near',
+        'of',
+        'off',
+        'on',
+        'out',
+        'over',
+        'through',
+        'to',
+        'toward',
+        'under',
+        'up',
+        'with',
+    ];
+
     public function titleCase($content)
     {
-      return $content;  
+        $title = [];
+
+        $content = ucwords($content);
+        $contentWords = explode(' ', $content);
+        $wordNumber = 1;
+        $wordTotal = count($contentWords);
+        foreach ($contentWords as $word) {
+            $wordLowercase = strtolower($word);
+
+            if ($wordNumber != 1 && $wordNumber != $wordTotal ) {
+                if (in_array($wordLowercase, $this->articles)) {
+                    $word = $wordLowercase;
+                }
+
+                if (in_array($wordLowercase, $this->conjunctions)) {
+                    $word = $wordLowercase;
+                }
+
+                if (in_array($wordLowercase, $this->prepositions)) {
+                    $word = $wordLowercase;
+                }
+            }
+
+            $title[] = $word;
+            $wordNumber += 1;
+        }
+
+        return implode(' ', $title);
     }
 
 }
--- tests/ContentTest.php
+++ tests/ContentTest.php
@@ -9,14 +9,60 @@ final class ContentTest extends TestCase
 {
     public function testTitleCase(): void
     {
-        // Sentence case 
-        $currentTitle = 'The strange case of Dr. Jekyll and Mr. Hyde';
-        $expected = 'The Strange Case of Dr. Jekyll and Mr. Hyde';
+        $currentTitle = 'burning chrome';
+        $expected = 'Burning Chrome';
 
         $content = new Content();
-        $resultTitle = $content->titleCase('Alice');
+        $resultTitle = $content->titleCase($currentTitle);
 
         $this->assertSame($expected, $resultTitle);
-
     }
+
+    public function testTitleCaseHasConjunctions(): void
+    {
+        // Sentence case
+        $currentTitle = 'writing for women and men yet nobody reads';
+        $expected = 'Writing for Women and Men yet Nobody Reads';
+
+        $content = new Content();
+        $resultTitle = $content->titleCase($currentTitle);
+
+        $this->assertSame($expected, $resultTitle);
+    }
+
+    public function testTitleCaseHasConjunctionsInitPreposition(): void
+    {
+        // Sentence case
+        $currentTitle = 'of mice and men';
+        $expected = 'Of Mice And Men';
+
+        $content = new Content();
+        $actual = $content->titleCase($currentTitle);
+
+        $this->assertSame('Of Mice And Men', $actual, 'These are the same');
+    }
+
+    public function testTitleCaseHasArticles(): void
+    {
+        // Sentence case
+        $currentTitle = 'the return of the king';
+        $expected = 'The Return of the King';
+
+        $content = new Content();
+        $resultTitle = $content->titleCase($currentTitle);
+
+        $this->assertSame($expected, $resultTitle);
+    }
+
+    public function testTitleCaseHasPrepositions(): void
+    {
+        $currentTitle = 'technology for, by, and of the people';
+        $expected = 'Technology for, by, and of the People';
+
+        $content = new Content();
+        $resultTitle = $content->titleCase($currentTitle);
+
+        $this->assertSame($expected, $resultTitle);
+    }
+
 }
